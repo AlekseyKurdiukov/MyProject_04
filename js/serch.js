6962bd190e24a9ed7d4ab80238220e1b4587a9fe
AlekseyKurdiukov <50284231+AlekseyKurdiukov@users.noreply.github.com>
2019-05-29 09:44:51 +0500

    Добавил функциональности search and login

--- js/serch.js
+++ js/serch.js
@@ -3,7 +3,6 @@ var serch_form = document.querySelector(".search__modal");
 var login_link = document.querySelector(".login");
 var login_form = document.querySelector(".login__modal");
 var login_form_input = document.querySelector(".login__modal_input");
-
 var email_login = login_form_input.querySelector("[name=email]");
 var password_login = login_form_input.querySelector("[name=password]");
 
@@ -20,17 +19,31 @@ login_link.addEventListener("click", function (evt) {
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (serch_form.classList.contains("show")) {
-      serch_form.classList.remove("show");
-      login_form.classList.remove("show");
-    }
+    serch_form.classList.remove("show");
+    login_form.classList.remove("show");
+    login_form.classList.remove("modal_error");
   }
 });
 
-// form__bg.addEventListener("click", function() {
-//   form__bg.classList.remove("show");
-// });
+$(".login").click(function() {
+  $('.login__modal').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".login_container").length) {
+    $('.login__modal').hide();
+  }
+  e.stopPropagation();
+});
 
+$(".search").click(function() {
+  $('.search__modal').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".search_container").length) {
+    $('.search__modal').hide();
+  }
+  e.stopPropagation();
+});
 
 login_form_input.addEventListener("submit", function (evt) {
   if (!email_login.value || !password_login.value) {
