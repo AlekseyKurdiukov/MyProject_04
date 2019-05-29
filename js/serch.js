b7b3f8e5ef0a0ee26c957bd4b99b2e43bd36ea49
AlekseyKurdiukov <50284231+AlekseyKurdiukov@users.noreply.github.com>
2019-05-29 13:14:34 +0500

    Сделал последнее модальное окно, но не понятна правильная функциональность

--- js/serch.js
+++ js/serch.js
@@ -1,5 +1,7 @@
 var serch_link = document.querySelector(".search");
 var serch_form = document.querySelector(".search__modal");
+var news_link = document.querySelector(".site_navigation__news_link");
+var news_form = document.querySelector(".site_navigation__news");
 var login_link = document.querySelector(".login");
 var login_form = document.querySelector(".login__modal");
 var login_form_input = document.querySelector(".login__modal_input");
@@ -16,10 +18,16 @@ login_link.addEventListener("click", function (evt) {
   login_form.classList.add("show");
 });
 
+news_link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  news_form.classList.add("show");
+});
+
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     serch_form.classList.remove("show");
+    news_form.classList.remove("show");
     login_form.classList.remove("show");
     login_form.classList.remove("modal_error");
   }
@@ -41,6 +49,16 @@ $(".search").click(function() {
 $(document).on('click', function(e) {
   if (!$(e.target).closest(".search_container").length) {
     $('.search__modal').hide();
+  }
+  e.stopPropagation();
+});
+
+$(".site_navigation__news_link").click(function() {
+  $('.site_navigation__news').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".site_navigation").length) {
+    $('.site_navigation__news').hide();
   }
   e.stopPropagation();
 });
