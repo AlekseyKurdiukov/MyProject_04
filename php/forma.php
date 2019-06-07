625534f5e20a18723dce28cbee531f8b9e3b1e39
AlekseyKurdiukov <50284231+AlekseyKurdiukov@users.noreply.github.com>
2019-06-07 13:04:20 +0500

    Работа с БД

--- php/forma.php
+++ php/forma.php
@@ -1,3 +1,19 @@
 <?php
-  echo print_r($_POST);
+
+  //Принимаю данные и записываю в переменные
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  //Проверка введенных данных
+  print_r ($_POST);
+  echo "<br />".$email."<br />".$password;
+  //Подключаюсь к БД и записываю информацию
+  $mysqli = new mysqli ("127.0.0.1", "root", "", "users");
+
+  $mysqli -> query ("SET NAMES 'utf8'");
+
+  $success =  $mysqli -> query ("INSERT INTO `user` (`id`, `login`, `password`, `data`) VALUES (NULL, '$email', MD5('$password'), UNIX_TIMESTAMP())");
+
+  $mysqli -> close ();
+  //Проверка работы функции
+  echo "<br />".$success;
 ?>
